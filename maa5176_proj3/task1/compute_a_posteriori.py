0ae1cd5739cc436256252f2fe0547b83a20f01ef
Mohammed Ahmed <mohammedaa30@hotmail.com>
2022-05-08 21:45:57 -0500

    Created print function, works with no input now too, moved some code around, store Qj in variable

--- maa5176_proj3/task1/compute_a_posteriori.py
+++ maa5176_proj3/task1/compute_a_posteriori.py
@@ -10,19 +10,42 @@ import sys
 def computeProbability():
     return 1
 
+def printProbability(h_values):
+    print("P(h1 | Q) = ", end="")
+    print(h_values[0])
+    print("P(h2 | Q) = ", end="")
+    print(h_values[1])
+    print("P(h3 | Q) = ", end="")
+    print(h_values[2])
+    print("P(h4 | Q) = ", end="")
+    print(h_values[3])
+    print("P(h5 | Q) = ", end="")
+    print(h_values[4])
 
 
-# Take in command line argument - Q is a string representing a series of observations ex. CLLCCCLLL
-Q = sys.argv[1] 
-length = 1#len(Q)                   # Length of Q
-observationCount = 0                # Observation Count
-h = {0.1, 0.2, 0.4, 0.2, 0.1}       # 5 types of bags of candy - 10% are h1, 20% are h2, etc...
-lime = {0, 0.25, 0.5, 0.75, 100}    # Percentage of lime candies in the bag
-cherry = {100, 0.75, 0.5, 0.25, 0}  # Percentage of cherry candies in the bag
+
+n = len(sys.argv)                       # number of arguments passed in
+
+observationCount = 0                    # Observation Count
+h = [0.1, 0.2, 0.4, 0.2, 0.1]           # 5 types of bags of candy - 10% are h1, 20% are h2, etc...
+lime = {0, 0.25, 0.5, 0.75, 100}        # Percentage of lime candies in the bag
+cherry = {100, 0.75, 0.5, 0.25, 0}      # Percentage of cherry candies in the bag
 
 
 # INITIAL
-currentProb = h                     # To start, probability values are same as hypothesis
+currentProb = h                         # To start, probability values are same as hypothesis
+prevProb = h                            # Initially, the probabilities are the same as the hypothesis
+
+# NO COMMANDS ENTERED
+if(n == 1):
+    print("No Observations Made\n")
+    printProbability(currentProb)       # If there are no commands entered, then the initial hypothesis is printed
+    quit()                              # End program
+
+
+# Q                                     # Take in command line argument - 
+Q = sys.argv[1]                         # Q is a string representing a series of observations ex. CLLCCCLLL                               
+length = 1#len(Q)                       # Length of Q
 
 
 
@@ -31,15 +54,16 @@ print("\nObservation sequence Q:", Q)
 print("Length of Q:", length)
 
 while(observationCount < length):
-    observationCount+=1
+    Qj = Q[observationCount]                                # Current letter being observed
+    observationCount+=1                                     # Increment observation count (starts at 1)
     print("\n\nAfter observation ", end="")
     print(observationCount, end="")
-    print(" = " + Q[observationCount-1] + ": ")
+    print(" = " + Qj + ": ")
 
     # Check whether observation is a lime or cherry
-    if (Q[observationCount-1] == 'C'):
+    if (Qj == 'C'):
         print("Cherry")
-    elif(Q[observationCount-1] == 'L'):
+    elif(Qj == 'L'):
         print("Lime")
 
 
